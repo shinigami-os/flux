628b2667a891f3a0089574da00bd7feddda98431
OxoGhost <arlenghost@proton.me>
2026-05-14 23:01:03 +0200

    remove function fully implemented

--- src/cmd_remove.c
+++ src/cmd_remove.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
+#include <unistd.h>
 #include "../include/flux.h"
 #include "../include/util.h"
 
 int flux_remove(int argc, char **argv, const char *usage) {
-    if (argc >= 1){
-        printf("[flux] would remove: %s\n", argv[0]);
-        return FLUX_ERR_NONE;
+    if (argc < 1) {
+        flux_usage_error(usage);
+        return FLUX_ERR_USAGE;
     }
-    flux_usage_error(usage);
-    return FLUX_ERR_USAGE;
+
+    const char *pkg = argv[0];
+    printf("[flux] removing: %s\n", pkg);
+
+    if (!flux_db_is_installed(pkg)) {
+        fprintf(stderr, "flux: %s is not installed\n", pkg);
+        return FLUX_ERR_NOT_FOUND;
+    }
+
+    if (flux_db_remove(pkg) != FLUX_ERR_NONE) {
+        fprintf(stderr, "flux: failed to remove %s\n", pkg);
+        return FLUX_ERR_GENERAL;
+    }
+
+    printf("[flux] %s removed successfully\n", pkg);
+    return FLUX_ERR_NONE;
 }
