087e50095805d6d8598ef6e1e55ccd7677a56060
OxoGhost <arlenghost@proton.me>
2026-05-15 09:58:46 +0200

    partially complete build function

--- src/cmd_build.c
+++ src/cmd_build.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include "../include/flux.h"
+#include "../include/util.h"
 
-int flux_build(int argc, char **argv, const char *usage){
-    (void)argc;
-    (void)argv;
-    (void)usage;
-    printf("[flux] stub: build not yet implemented");
-    return FLUX_ERR_NONE;
+extern int flux_install(int argc, char **argv, const char *usage);
+
+int flux_build(int argc, char **argv, const char *usage) {
+    if (argc < 1) {
+        flux_usage_error(usage);
+        return FLUX_ERR_USAGE;
+    }
+    printf("[flux] force building: %s\n", argv[0]);
+    printf("[flux] note: flux build will diverge from flux install once binary cache is implemented\n");
+    // reuse install logic for now
+    return flux_install(argc, argv, usage);
 }
